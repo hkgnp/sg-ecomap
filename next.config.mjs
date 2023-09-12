c7c0ea297aa646d9b6a6339597fb0bde1b6403eb
hkgnp <github.com/hkgnp>
2023-09-13 07:42:26 +0800

    please compile

--- next.config.mjs
+++ next.config.mjs
@@ -11,18 +11,18 @@ const ContentSecurityPolicy = `
   form-action 'self';
   frame-ancestors 'self';
   img-src 'self' data: blob: ${
-  // For displaying images from R2
-  env.R2_PUBLIC_HOSTNAME ? `https://${env.R2_PUBLIC_HOSTNAME}` : ''
+    // For displaying images from R2
+    env.R2_PUBLIC_HOSTNAME ? `https://${env.R2_PUBLIC_HOSTNAME}` : ''
   };
   frame-src 'self';
   object-src 'none';
   script-src 'self' ${env.NODE_ENV === 'production' ? '' : "'unsafe-eval'"};
   style-src 'self' https: 'unsafe-inline';
   connect-src 'self' https://*.browser-intake-datadoghq.com https://vitals.vercel-insights.com/v1/vitals ${
-  // For POSTing presigned URLs to R2 storage.
-  env.R2_ACCOUNT_ID
-    ? `https://*.${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`
-    : ''
+    // For POSTing presigned URLs to R2 storage.
+    env.R2_ACCOUNT_ID
+      ? `https://*.${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`
+      : ''
   };
   worker-src 'self' blob:;
   ${env.NODE_ENV === 'production' ? 'upgrade-insecure-requests' : ''}
@@ -34,6 +34,9 @@ const ContentSecurityPolicy = `
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /**
    * Dynamic configuration available for the browser and server.
    * Note: requires `ssr: true` or a `getInitialProps` in `_app.tsx`
@@ -43,7 +46,7 @@ const config = {
     NODE_ENV: env.NODE_ENV,
   },
   /** We run eslint as a separate task in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
+  //eslint: { ignoreDuringBuilds: !!process.env.CI },
   images: {
     domains: [env.R2_PUBLIC_HOSTNAME ?? ''].filter((d) => d),
   },
