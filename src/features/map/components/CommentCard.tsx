cabbba5d0f90477c3704afdade32eede0135de16
hkgnp <github.com/hkgnp>
2023-09-14 20:03:05 +0800

    feat: styled up comment cards

--- src/features/map/components/CommentCard.tsx
+++ src/features/map/components/CommentCard.tsx
@@ -1,6 +1,16 @@
 import { format } from 'date-fns'
 import { CommentCardProps } from '../types'
-import { Text } from '@chakra-ui/react'
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from '@chakra-ui/react'
+import { RxAvatar } from 'react-icons/rx'
 
 export const CommentCard = ({
   title,
@@ -10,12 +20,26 @@ export const CommentCard = ({
 }: CommentCardProps) => {
   return (
     <>
-      <Text textStyle="body-2">{title}</Text>
-      <Text textStyle="body-2">{content}</Text>
-      <Text textStyle="body-2">author: {authorName}</Text>
-      <Text textStyle="caption">
-        created: {format(new Date(updatedAt), 'dd-MM-yyyy')}
-      </Text>
+      <Card marginBottom="4">
+        <CardBody>
+          <Heading size="xs" textTransform="uppercase" marginBottom="2">
+            {title}
+          </Heading>
+          <Stack divider={<StackDivider />} spacing="3">
+            <Box>
+              <Text textStyle="body-2">{content}</Text>
+            </Box>
+            <Flex flexDirection="row" justifyContent="space-between">
+              <Text textStyle="caption-1" display="flex" gap="1">
+                <RxAvatar size="14" /> {authorName}
+              </Text>
+              <Text textStyle="caption-1">
+                {format(new Date(updatedAt), 'dd-MM-yyyy')}
+              </Text>
+            </Flex>
+          </Stack>
+        </CardBody>
+      </Card>
     </>
   )
 }
--- src/features/map/components/InfoBarDetails.tsx
+++ src/features/map/components/InfoBarDetails.tsx
@@ -3,7 +3,6 @@ import { BiGlobe, BiPhone, BiMailSend, BiMapPin } from 'react-icons/bi'
 import { ResourceWithPosts } from '../types'
 
 export const InfoBarDetails = ({
-  id,
   address,
   postalCode,
   website,
@@ -19,26 +18,47 @@ export const InfoBarDetails = ({
         </Text>
       </Flex>
 
-      <Flex gap="2">
-        <BiMailSend size="24px" />
-        <Text textStyle="body-2" noOfLines={1} lineHeight="2" marginBottom="2">
-          google@google.com
-        </Text>
-      </Flex>
+      {email && (
+        <Flex gap="2">
+          <BiMailSend size="24px" />
+          <Text
+            textStyle="body-2"
+            noOfLines={1}
+            lineHeight="2"
+            marginBottom="2"
+          >
+            {email}
+          </Text>
+        </Flex>
+      )}
 
-      <Flex gap="2">
-        <BiPhone size="24px" />
-        <Text textStyle="body-2" noOfLines={1} lineHeight="2" marginBottom="2">
-          62341239 / 91234567
-        </Text>
-      </Flex>
+      {contactNumber && (
+        <Flex gap="2">
+          <BiPhone size="24px" />
+          <Text
+            textStyle="body-2"
+            noOfLines={1}
+            lineHeight="2"
+            marginBottom="2"
+          >
+            {contactNumber}
+          </Text>
+        </Flex>
+      )}
 
-      <Flex gap="2">
-        <BiGlobe size="24px" />
-        <Text textStyle="body-2" noOfLines={1} lineHeight="2" marginBottom="2">
-          https://www.google.com
-        </Text>
-      </Flex>
+      {website && (
+        <Flex gap="2">
+          <BiGlobe size="24px" />
+          <Text
+            textStyle="body-2"
+            noOfLines={1}
+            lineHeight="2"
+            marginBottom="2"
+          >
+            {website}
+          </Text>
+        </Flex>
+      )}
     </>
   )
 }
