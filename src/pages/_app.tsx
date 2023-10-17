b4167c01203403468e0b8502067e50b671bc5eea
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-17 20:40:04 +0800

    fix: leaflet control hack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@opengovsg/design-system-react";
 import { theme } from "../theme";
+import "inter-ui/inter.css";
+import "../features/map/styles/leaflet-control-hack.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
