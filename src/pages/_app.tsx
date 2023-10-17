97e53084655923124b6c3c5ea2f1ccd639d3750c
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-17 20:14:19 +0800

    chore: add theme, eslint plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@opengovsg/design-system-react";
+import { theme } from "../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />;
     </ThemeProvider>
   );
