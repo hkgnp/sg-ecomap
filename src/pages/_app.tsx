9ab53c9b5d950cb1902aaa710efa92d0e6d8c481
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-17 20:53:10 +0800

    fix: autofocus remove

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,19 @@ import { ThemeProvider } from "@opengovsg/design-system-react";
 import { theme } from "../theme";
 import "inter-ui/inter.css";
 import "../styles/leaflet-control-hack.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>SG ECOMAP</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
       <Component {...pageProps} />;
     </ThemeProvider>
   );
