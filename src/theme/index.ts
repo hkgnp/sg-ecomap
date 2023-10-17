e6dbe1ea072c2ba3a929a4a93d37049d5c8b9297
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-17 20:25:35 +0800

    chore: add theme, eslint plugin

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,16 +1,14 @@
-import { extendTheme } from '@chakra-ui/react'
-import { theme as ogpDsTheme } from '@opengovsg/design-system-react'
-import { shadows } from './foundations/shadows'
-import { layerStyles } from './layerStyles'
-import { components } from './components'
-import { colors } from './foundations/colors'
+import { extendTheme } from "@chakra-ui/react";
+import { theme as ogpDsTheme } from "@opengovsg/design-system-react";
+import { shadows } from "./foundations/shadows";
+import { layerStyles } from "./layerStyles";
+import { colors } from "./foundations/colors";
 
 export const theme = extendTheme(ogpDsTheme, {
   shadows,
   colors,
   components: {
     ...ogpDsTheme.components,
-    ...components,
   },
   layerStyles,
-})
+});
