3805c3e5a791a441c847bfee47fdbf2eaa8fdb9d
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-20 00:03:47 +0800

    chore: cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,47 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
 const config = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    project: path.join(__dirname, "tsconfig.json"),
   },
-  plugins: ["@typescript-eslint"],
-  ignorePatterns: ["!.*", "dist", "node_modules"],
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+  ],
   rules: {
-    "@typescript-eslint/ban-ts-comment": "off",
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: [
+          {
+            name: "react",
+            importNames: ["Suspense"],
+            message:
+              "Please use Suspense from /components instead. Default React 18 Suspense is not supported for SSR",
+          },
+        ],
+      },
+    ],
+    "@typescript-eslint/no-floating-promises": "error",
     "prettier/prettier": "error",
-    "comma-dangle": ["error", "always-multiline"],
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
@@ -35,5 +51,4 @@ const config = {
     ],
   },
 };
-
 module.exports = config;
