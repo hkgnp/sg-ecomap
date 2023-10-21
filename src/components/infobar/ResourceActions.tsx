c8f409d6709575eac8565284b5f931eef5de7805
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-21 13:08:27 +0800

    feat: submit new resource link

--- src/components/infobar/ResourceActions.tsx
+++ src/components/infobar/ResourceActions.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Wrap, WrapItem } from "@chakra-ui/react";
 import { Tag } from "@opengovsg/design-system-react";
 import { ResourceActionProps } from "../types";
 import { useState } from "react";
@@ -31,24 +31,46 @@ export const ResourceActions = ({
   };
 
   return (
-    <Flex direction="row" marginTop="4" gap="3">
-      <Tag size="xs" cursor="pointer" colorScheme="main">
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href={`${process.env.NEXT_PUBLIC_FORMSG_LINK}?${process.env.NEXT_PUBLIC_FORMSG_FIELD_ID}=${resource.name}`}
+    <Wrap marginTop="4" spacing="2" align="center">
+      <WrapItem>
+        <Tag size="xs" cursor="pointer" colorScheme="main">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`${process.env.NEXT_PUBLIC_FORMSG_LINK}?${process.env.NEXT_PUBLIC_FORMSG_FIELD_ID}=${resource.name}`}
+          >
+            Submit an Edit
+          </a>
+        </Tag>
+      </WrapItem>
+
+      <WrapItem>
+        <Tag
+          onClick={handleInactive}
+          size="xs"
+          cursor="pointer"
+          colorScheme="critical"
         >
-          Submit an Edit
-        </a>
-      </Tag>
-      <Tag
-        onClick={handleInactive}
-        size="xs"
-        cursor="pointer"
-        colorScheme="critical"
-      >
-        {inactive ? "Mark as Active" : "Mark as Inactive"}
-      </Tag>
-    </Flex>
+          {inactive ? "Mark as Active" : "Mark as Inactive"}
+        </Tag>
+      </WrapItem>
+
+      <WrapItem>
+        <Tag
+          size="xs"
+          cursor="pointer"
+          colorScheme="success"
+          textAlign="center"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={process.env.NEXT_PUBLIC_FORMSG_LINK}
+          >
+            Suggest New Resource
+          </a>
+        </Tag>
+      </WrapItem>
+    </Wrap>
   );
 };
