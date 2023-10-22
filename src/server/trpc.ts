b53417a7e71f8e19744b18fc1a62f075467fef3c
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-22 18:30:15 +0800

    feat: trpc for resources

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,13 +1,10 @@
-import { inferAsyncReturnType, initTRPC } from "@trpc/server";
-import { createContext } from "./context";
+import { initTRPC } from "@trpc/server";
 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC
-  .context<inferAsyncReturnType<typeof createContext>>()
-  .create();
+const t = initTRPC.create();
 
 // Base router and procedure helpers
 export const router = t.router;
