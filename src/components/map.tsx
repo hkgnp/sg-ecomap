1afb09c5ba458dc9bba0533f4c088a893302b9b2
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-24 14:30:06 +0800

    feat: change position of leaflet control

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -16,11 +16,11 @@ const Index = () => {
       scrollWheelZoom={false}
       style={{ position: "fixed", height: "100vh", width: "100%" }}
     >
-      <ZoomControl position="bottomleft" />
+      <ZoomControl position="bottomright" />
       <TileLayer
         url={"https://www.onemap.gov.sg/maps/tiles/Default/{z}/{x}/{y}.png"}
         attribution={
-          '<img src="https://www.onemap.gov.sg/web-assets/images/logo/om_logo.png" style="height:20px;width:20px;" alt="Attribution"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
+          '<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
         }
       />
       <UtilityLayer />
